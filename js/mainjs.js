85dbbbab7c68174170c83f45a908d75ea1edc67a
Arman <walvorin@gmail.com>
2019-06-23 23:37:04 +0600

    9. Введение в JavaScript

---
+++ js/mainjs.js
@@ -0,0 +1,55 @@
+var link = document.querySelector(".lead-form__start");
+var hotelPopup = document.querySelector(".lead-form__popup");
+
+link.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    hotelPopup.classList.add("lead-form__popup_show");
+    console.log("Нажата кнопка регистрации в гостинице");
+    
+});
+
+
+/*
+
+var link = document.querySelector(".lead-form__start");
+var hotelPopup = document.querySelector(".lead-form__popup");
+var close = document.querySelector(".modal-close"); //Если есть кнопка закрыть
+var login = hotelPopup.querySelector("[name="date-come"]");
+
+link.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    hotelPopup.classList.add("lead-form__popup_show");
+    login.focus();
+    console.log("Нажата кнопка регистрации в гостинице");
+    
+})
+
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    hotelPopup.classList.remove("modal-show")
+})
+
+
+*/
+
+/*
+
+var link = document.querySelector(".lead-form__start");
+var hotelPopup = document.querySelector(".lead-form__popup");
+
+var login = hotelPopup.querySelector("[name="date-come"]");
+
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    hotelPopup.classList.add("lead-form__popup_show");
+    login.focus();
+})
+
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    hotelPopup.classList.remove("lead-form__popup_show");
+
+});
+
+
+*/
