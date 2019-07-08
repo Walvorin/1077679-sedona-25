749976ad6566e070139133a10121ffcd167ad334
Arman <walvorin@gmail.com>
2019-07-09 00:24:29 +0600

    Test Final 1

--- js/mainjs.js
+++ js/mainjs.js
@@ -1,56 +1,147 @@
-var buttonShowForm = document.querySelector(".lead-form__start");
 
-var openForm = document.querySelector(".lead-form__popup");
-var form = document.querySelector("form");
 
-var dateInCome = document.querySelector("[id=date-come]");
+//var btnSearchForm = document.querySelector(".lead-form__start"); 
+//var showForm = document.querySelector(".lead-form__popup");
+//
+//var map = document.querySelector(".map-navigation");
+//var dateCome = showForm.querySelector("[name=date-come]");
+//var dateOut = showForm.querySelector("[name=date-out]");
+//var totalAdult = showForm.querySelector("[name=adult]");
+//var totalChild = showForm.querySelector("[name=children]");
+//
+//var isStorageSupport = true;
+//var storage1 = "";
+//var storage2 = "";
+//var storage3 = "";
+//var storage4 = "";
+//
+//try {
+//   storage1  = localStorage.getItem("dateCome");
+//   storage2  = localStorage.getItem("dateOut");
+//   storage3  = localStorage.getItem("totalAdult");
+//   storage4  = localStorage.getItem("totalChild");
+// } catch (err) {
+//   isStorageSupport = false;
+// }
+//
+//showForm.classList.add("form-close");
+//
+//btnSearchForm.addEventListener("click", function (evt) {
+//    evt.preventDefault();
+//    showForm.classList.toggle("form-show");
+//  });
+//
+//
+//
+//window.addEventListener("keydown", function (evt) {
+//    if (evt.keyCode === 27) {
+//      evt.preventDefault();
+//      if (showForm.classList.contains("form-close")) {
+//        showForm.classList.remove("form-show");
+//        showForm.classList.remove("enter-error");
+//      }
+//    }
+//  });
+//
+//showForm.addEventListener("submit", function(evt){
+//    evt.preventDefault();
+//});
 
-var dateOutCome  = document.querySelector("[id=date-out]");
 
-var adult = openForm.querySelector("[name=adult]");
-var children = openForm.querySelector("[name=children]");
 
-var popupClose = document.querySelector(".map-navigation");
 
+
+
+var btnSearchForm = document.querySelector(".lead-form__start"); 
+var showForm = document.querySelector(".lead-form__popup");
+
+var map = document.querySelector(".map-navigation");
+var dateCome = showForm.querySelector("[name=date-come]");
+var dateOut = showForm.querySelector("[name=date-out]");
+var totalAdult = showForm.querySelector("[name=adult]");
+var totalChild = showForm.querySelector("[name=children]");
 
 var isStorageSupport = true;
-var storage = "";
+var storage1 = "";
+var storage2 = "";
+var storage3 = "";
+var storage4 = "";
 
 try {
-   storage = localStorage.getItem("adult");
-   storage = localStorage.getItem("children");
+   storage1  = localStorage.getItem("dateCome");
+   storage2  = localStorage.getItem("dateOut");
+   storage3  = localStorage.getItem("totalAdult");
+   storage4  = localStorage.getItem("totalChild");
  } catch (err) {
    isStorageSupport = false;
  }
-                                      
-                                      
 
-buttonShowForm.addEventListener("click", function(evt) {
+showForm.classList.add("form-close");
+
+btnSearchForm.addEventListener("click", function(evt){
     evt.preventDefault();
-    openForm.classList.toggle("lead-form__popup_show");
-    popupClose.classList.remove("lead-form__popup_show");
-    
-    if(storage) {
-        dateInCome.value = storage;
-        dateOutCome.focus();
-    } else {
-        dateInCome.focus();
-    }
+    showForm.classList.toggle("form-show");
+    dateCome.focus();
+    if (storage1) {
+        dateCome.value = storage1;
+        dateOut.focus();
+    } else if (storage2) {
+        dateOut.value = storage2;
+        totalAdult.focus();   
+    } else if (storage3) {
+        totalAdult.value = storage3;
+        totalChild.focus(); 
+    } else if (storage4) {
+        totalChild.value = storage4;
+        totalChild.focus();
+    };
 });
 
 
-
-
-
-form.addEventListener("submit"), function(evt) {
-    
-    if (!dateInCome.value || !dateOutCome.value){
+showForm.addEventListener("submit", function(evt){
+    if(!dateCome.value) {
         evt.preventDefault();
-        console.log("Нужно ввести дату заезда и выезда");
-    }  else {
-        if (isStorageSupport) {
-            localStorage.setItem("dateIn", "dateInCome")
-            }
-        
+//        console.log("Вы забыли ввести поле Въезда");
+        dateCome.classList.add("enter-error");
+//        dateCome.focus();
+    } else {
+        localStorage.setItem("dateCome", dateCome.value);
+        dateCome.classList.remove("enter-error");
+    };
+    if (!dateOut.value) {
+        evt.preventDefault();
+//        console.log("Вы забыли ввести поле Выезда");
+        dateOut.classList.add("enter-error");
+    } else {
+        localStorage.setItem("dateOut", dateOut.value);
+        dateOut.classList.remove("enter-error");
+    };
+    if (!totalAdult.value) {
+        evt.preventDefault();
+//        console.log("Вы забыли ввести поле Выезда");
+        totalAdult.classList.add("enter-error");
+//        dateOut.focus();
+    } else {
+        localStorage.setItem("totalAdult", totalAdult.value);
+    };
+    if (!totalChild.value) {
+        evt.preventDefault();
+//        console.log("Вы забыли ввести поле Выезда");
+        totalChild.classList.add("enter-error");
+//        dateOut.focus();
+    } else {
+        localStorage.setItem("totalChild", totalChild.value);
+    };
+    
+});
+
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (showForm.classList.contains("form-close")) {
+        showForm.classList.remove("form-show");
+        showForm.classList.remove("enter-error");
+      }
     }
-}
+  });
